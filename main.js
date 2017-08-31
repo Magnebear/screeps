182339f555ef720f8c510fb3eccb1a9ceb0c4014
Magnebear <github.mail@cyfor.no>
2017-08-31 20:09:44 +0200

    a

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var roleRepair = require('role.repair');
 
 var maxHarvester = 1;
 var maxBuilders = 2;
-var maxUpgraders = 4;
+var maxUpgraders = 10;
 var maxMegaMiners1 = 1
 var maxMegaMiners2 = 1
 var maxTransporters = 4;
