fbbdb389ab16cbde5319d05007aecd67145579eb
Magnebear <github.mail@cyfor.no>
2017-09-01 12:28:29 +0200

    Update role.claimer.js

--- role.claimer.js
+++ role.claimer.js
@@ -4,7 +4,7 @@ var roleClaimer = {
   run: function(creep) {
     var cont = Game.getObjectById("59830062b097071b4adc42d7");
     if(creep.reserveController(cont) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(cont);
+      creep.travelTo(cont);
     }
   }
 };
