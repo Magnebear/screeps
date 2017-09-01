d44ef0b58cd20468b056d4ee608944424ec165e3
Magnebear <github.mail@cyfor.no>
2017-09-02 00:15:07 +0200

    a

--- main.js
+++ main.js
@@ -55,6 +55,7 @@ module.exports.loop = function () {
         console.log('repair: ' + repairs.length);
 		console.log('externalHarvesters: ' + externalHarvesters.length);
 		console.log('-------------------------------')
+		linkController.run("59a9d6c7901b9f6272a9c69a", "59a9ca4e83bd410897a24445")
         Memory.clock = 0;
     }
 
@@ -127,7 +128,7 @@ module.exports.loop = function () {
     
 	towerController.run(Game.getObjectById('59a48e720033416e2fa8ea27'));
 	towerController.run(Game.getObjectById('59a9bd8b58bf8523b6247f27'));
-	linkController.run("59a9d6c7901b9f6272a9c69a", "59a9ca4e83bd410897a24445")
+	
 	
 	
     for(var name in Game.creeps) {
