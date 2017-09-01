bdc3cd4aa61a2f8da9fbc1bcc98856b56afe225a
Magnebear <github.mail@cyfor.no>
2017-09-02 00:21:32 +0200

    a

--- LinkController.js
+++ LinkController.js
@@ -2,7 +2,7 @@ var linkControll = {
     run: function(o,d) {
 		originLink = Game.getObjectById("59a9d6c7901b9f6272a9c69a");
 		destinationLink = Game.getObjectById("59a9ca4e83bd410897a24445");
-
+		console.log(o.store);
 		if(o.energy == 800){
 			o.transferEnergy(d);
 			console.log("Energy transfered!");
