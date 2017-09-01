aa83ec94325e0632a615aa9125e99dd962130bf3
Magnebear <github.mail@cyfor.no>
2017-09-01 19:44:30 +0200

    a

--- role.builder.js
+++ role.builder.js
@@ -28,7 +28,12 @@ var roleBuilder = {
 			var bC = Game.getObjectById("59a7c22c82c55314c9f9a863");
             if(creep.withdraw(bC, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                creep.travelTo(bC);
-            }
+            } else {
+				var c = Game.getObjectById("59a833729347b91c822b50ba")
+				if(c.store[RESOURCE_ENERGY] > 0 || creep.withdraw(c, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+						creep.travelTo(c);
+				}
+			}
 	    }
 	}
 };
