99b31fac7075a2021b143fa74aa5bebb9325bb88
Magnebear <github.mail@cyfor.no>
2017-09-02 19:33:52 +0200

    aa

--- role.harvester.js
+++ role.harvester.js
@@ -4,9 +4,11 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.memory.harvesting){
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.travelTo(sources[0]);
+            var source = Game.getObjectById("59830062b097071b4adc42da");
+            if(!source){
+                creep.travelTo(Game.flags.claimTarget01);
+            } else if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.travelTo(source);
             }
             if(creep.carry.energy==creep.carryCapacity){
                 creep.memory.harvesting = false;
@@ -15,21 +17,9 @@ var roleHarvester = {
             if(creep.carry.energy==0){
                 creep.memory.harvesting = true;
             }
-	        var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER)
-                        && structure.energy < structure.energyCapacity;
-                    }
-            });
-            targets.sort();
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.travelTo(targets[0]);
-                }
-            } else {
-                creep.travelTo(Game.flags.HoldingArea);
+	        var target = Game.getObjectById("59830062b097071b4adc42db");
+            if(creep.upgradeController(target) == ERR_NOT_IN_RANGE) {
+                    creep.travelTo(target);
             }
         }
 	}
