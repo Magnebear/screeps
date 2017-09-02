2fa2b125da3efd292e1f73a8880d6568ad568176
Magnebear <github.mail@cyfor.no>
2017-09-02 10:26:20 +0200

    a

--- cont.towerController.js
+++ cont.towerController.js
@@ -5,7 +5,7 @@ var towerControll = {
 		console.log("Hostile:",closestHostile);
 		if(closestHostile) {
 			console.log("Attacking!");
-			tower.attack(closestHostile);
+			console.log(tower.attack(closestHostile);
 		}
 		var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
 			filter: (structure) =>
