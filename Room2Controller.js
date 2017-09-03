837a9ef67e16155804da4a7b673b11c7c2542479
Magnebear <github.mail@cyfor.no>
2017-09-03 11:17:52 +0200

    a

--- Room2Controller.js
+++ Room2Controller.js
@@ -1,6 +1,20 @@
 var room2Controller = {
     run: function() {
-
+	if(Game.time%5 == 0){
+		var tock = true;
+	} else {
+		var tock = false;
+	}
+	
+	if(tock){
+			console.log("Room 2 controller running!");
+	}
+	
+	
+	
+	
+	
+	
 
 	}
 };
