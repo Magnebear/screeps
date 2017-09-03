386e04c25b5b3bd2be2effb85091b2dcb10ebfc2
Magnebear <github.mail@cyfor.no>
2017-09-03 11:28:17 +0200

    a

--- Room2Controller.js
+++ Room2Controller.js
@@ -9,8 +9,10 @@ var room2Controller = {
 		if(tock){
 				console.log("Room 2 controller running @ ", Game.time);
 		}
-		
+		    const startCpu = Game.cpu.getUsed();
+
 		Game.spawns["Spawn2"].room.find(FIND_MY_CREEPS);
+		    console.log('elapsed:', Game.cpu.getUsed() - startCpu;);
 	}
 };
 
