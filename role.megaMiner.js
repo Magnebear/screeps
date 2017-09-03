40818b19341ef11d006b9d227f300a2f9815a695
Magnebear <github.mail@cyfor.no>
2017-09-03 12:23:02 +0200

    a

--- role.megaMiner.js
+++ role.megaMiner.js
@@ -4,7 +4,6 @@ var roleMegaMiner = {
     /** @param {Creep} creep **/
     run: function(creep) {
         var container = Game.getObjectById(creep.memory.contID);
-		console.log(container);
 		if(container.store[RESOURCE_ENERGY] < 2000){
 			if(creep.harvest(Game.getObjectById(creep.memory.srcID)) == ERR_NOT_IN_RANGE) {
 				creep.travelTo(container);
