2aea0c48c1b910490762c94e1b29ec852ec1f57e
Magnebear <github.mail@cyfor.no>
2017-09-04 21:22:55 +0200

    a

--- role.builder.js
+++ role.builder.js
@@ -1,14 +1,12 @@
 var Traveler = require('Traveler');
 
 var roleBuilder = {
-
     run: function(creep) {
 		if(creep.memory.container){
 			var container = Game.getObjectById(creep.memory.container);
 		} else {
 			var container = Game.getObjectById("59a833729347b91c822b50ba");
 		}
-		
 		
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
