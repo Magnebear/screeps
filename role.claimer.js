473c82e01ee2220c9ae9eda73a316a0309a2f787
Magnebear <github.mail@cyfor.no>
2017-09-08 22:31:09 +0200

    a

--- role.claimer.js
+++ role.claimer.js
@@ -4,7 +4,7 @@ var roleClaimer = {
   run: function(creep) {
     var targetController = Game.getObjectById(creep.memory.targetController);
 	var targetFlag = Game.flags[creep.memory.targetFlag]
-	console.log(creep.reserveController(targetController))
+
     if(creep.reserveController(targetController) != 0) {
       creep.travelTo(targetFlag);
     }
