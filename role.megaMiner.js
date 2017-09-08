43aaf57a96dbd689338f5b0e26fd0f2b91fd8580
Magnebear <github.mail@cyfor.no>
2017-09-08 23:23:30 +0200

    a

--- role.megaMiner.js
+++ role.megaMiner.js
@@ -28,11 +28,11 @@ var roleMegaMiner = {
                 creep.repair(container);
             }
         }
-    };
+    }
 	create: function (name, creepBody, sourceID, containerID, spawn){
 		var newName = Game.spawns[spawn].createCreep(creepBody, name, {role: 'megaMiner', srcID:sourceID, contID:containerID});
 		return newName;
-	};
+	}
 };
 
 module.exports = roleMegaMiner;
