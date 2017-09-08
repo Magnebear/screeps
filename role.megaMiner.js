5618c8aba6f2f125bee3fe3cf33f3f9d2b34a0eb
Magnebear <github.mail@cyfor.no>
2017-09-08 23:24:31 +0200

    a

--- role.megaMiner.js
+++ role.megaMiner.js
@@ -2,6 +2,10 @@ var Traveler = require('Traveler');
 
 var roleMegaMiner = {
     /** @param {Creep} creep **/
+	create: function (name, creepBody, sourceID, containerID, spawn){
+		var newName = Game.spawns[spawn].createCreep(creepBody, name, {role: 'megaMiner', srcID:sourceID, contID:containerID});
+		return newName;
+	}
     run: function(creep) {
         var container = Game.getObjectById(creep.memory.contID);
         var source = Game.getObjectById(creep.memory.srcID);
@@ -29,10 +33,7 @@ var roleMegaMiner = {
             }
         }
     }
-	create: function (name, creepBody, sourceID, containerID, spawn){
-		var newName = Game.spawns[spawn].createCreep(creepBody, name, {role: 'megaMiner', srcID:sourceID, contID:containerID});
-		return newName;
-	}
+
 };
 
 module.exports = roleMegaMiner;
