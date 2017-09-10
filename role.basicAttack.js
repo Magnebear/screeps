058bb885415372a285b7a3077763f77821db036e
Magnebear <github.mail@cyfor.no>
2017-09-10 13:26:22 +0200

    a

--- role.basicAttack.js
+++ role.basicAttack.js
@@ -4,33 +4,30 @@ var body = [MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTA
 module.exports  = {
     /** @param {Creep} creep **/
     run: function(creep) {
-		if(!Game.flags[creep.memory.targetFlag].room){
-			creep.travelTo(Game.flags[creep.memory.targetFlag]);
-		} else {
-			//Get target by priority
-			var closestExtension = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
-				filter: (structure) =>
-				structure.structureType == STRUCTURE_EXTENSION
-			});
-			var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 
-			var closestStructure = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
-			
-			if(closestExtension){
-				if(creep.attack(closestExtension) == ERR_NOT_IN_RANGE){
-					creep.travelTo(closestExtension);
-				}
-			} else if(closestHostile) {
-				if(creep.attack(closestHostile) == ERR_NOT_IN_RANGE){
-					creep.travelTo(closestHostile);
-				}
-			} else if(closestStructure) {
-				if(creep.attack(closestStructure) == ERR_NOT_IN_RANGE){
-					creep.travelTo(closestStructure);
-				}
-			} else {
-				creep.travelTo(Game.flags[creep.memory.targetFlag]);
+		//Get target by priority
+		var closestExtension = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
+			filter: (structure) =>
+			structure.structureType == STRUCTURE_EXTENSION
+		});
+		var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+
+		var closestStructure = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+		
+		if(closestExtension){
+			if(creep.attack(closestExtension) == ERR_NOT_IN_RANGE){
+				creep.travelTo(closestExtension);
 			}
+		} else if(closestHostile) {
+			if(creep.attack(closestHostile) == ERR_NOT_IN_RANGE){
+				creep.travelTo(closestHostile);
+			}
+		} else if(closestStructure) {
+			if(creep.attack(closestStructure) == ERR_NOT_IN_RANGE){
+				creep.travelTo(closestStructure);
+			}
+		} else {
+			creep.travelTo(Game.flags[creep.memory.targetFlag]);
 		}
     },
 	create: function (name, flag, spawn){
