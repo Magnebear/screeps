b38aedf3d13ecbf6bdce1b4de30e2f482ca4138e
Magnebear <github.mail@cyfor.no>
2017-09-10 13:35:45 +0200

    a

--- main.js
+++ main.js
@@ -210,11 +210,14 @@ module.exports.loop = function () {
     
 	towerController.run(Game.getObjectById('59a9bd8b58bf8523b6247f27'));
 	towerController.run(Game.getObjectById('59aa6fc87073420285cddfe8'));
+	
 	towerController.run(Game.getObjectById('59ac591cd00bbd1ef4dbf699'));
     towerController.run(Game.getObjectById('59b15159f2a0fc1679a26f52'));
     
 	linkController.run("59a9d6c7901b9f6272a9c69a", "59a9ca4e83bd410897a24445");
 	linkController.run("59aea243daa30c78a13596a1", "59a9ca4e83bd410897a24445");
+
+	linkController.run("59b15a5a59a2a537e2656960", "59b15c08a6e4ec01b7ef900c");
 	
 	room2Controller.run();
 	
