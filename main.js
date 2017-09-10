71170166e07c6a9d30a59a2c95fd28268fa60d94
Magnebear <github.mail@cyfor.no>
2017-09-10 13:21:13 +0200

    a

--- main.js
+++ main.js
@@ -145,7 +145,7 @@ module.exports.loop = function () {
 			console.log('Spawning new externalHarvester2: ' + newName);
 		}
 		
-		if (Game.creeps["claimer1"] == undefined){
+		if (Game.creeps["claimer1"] == undefined && Game.creeps.claimer1.room.controller.reservation.ticksToEnd < 1500){
 				//Spawn new claimer 1
 				var newName = Game.spawns['Spawn1'].createCreep(claimerCreep, "claimer1", 
 					{role: "claimer",
@@ -154,7 +154,7 @@ module.exports.loop = function () {
 				});
 				console.log("Spawning new claimer 1");
 		}
-		if (Game.creeps["claimer2"] == undefined){
+		if (Game.creeps["claimer2"] == undefined && Game.creeps.claimer2.room.controller.reservation.ticksToEnd < 1500){
 				//Spawn new claimer 2
 				var newName = Game.spawns['Spawn1'].createCreep(claimerCreep, "claimer2", 
 					{role: "claimer",
