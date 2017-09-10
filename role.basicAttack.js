5735a5cce725c2f99d9494bf2439d9b34ddf020b
Magnebear <github.mail@cyfor.no>
2017-09-10 14:16:22 +0200

    a

--- role.basicAttack.js
+++ role.basicAttack.js
@@ -4,33 +4,31 @@ var body = [MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTA
 module.exports  = {
     /** @param {Creep} creep **/
     run: function(creep) {
-		if(creep.room.name == "E43S28") {
-			var target = undefined;
-			//Get target by priority
-			var closestExtension = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
-				filter: (structure) =>
-				structure.structureType == STRUCTURE_EXTENSION
-			});
-			var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-			var closestStructure = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
-			
-			if(creep.memory.target){
-				target = Game.getObjectById(creep.memory.targetId);
-			} else if(closestExtension){
-				target = closestExtension;
-			} else if(closestHostile) {
-				target = closestHostile;
-			} else if(closestStructure) {
-				target = closestStructure;
-			} else {
-				target = false;
-			}
-			if(target){
-				if(creep.attack(target) == ERR_NOT_IN_RANGE){
-					creep.moveTo(target);
-				} 
-				creep.memory.targetId = target.id;
-			}
+		var target = undefined;
+		//Get target by priority
+		var closestExtension = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
+			filter: (structure) =>
+			structure.structureType == STRUCTURE_EXTENSION
+		});
+		var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+		var closestStructure = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+		
+		if(creep.memory.target){
+			target = Game.getObjectById(creep.memory.targetId);
+		} else if(closestExtension){
+			target = closestExtension;
+		} else if(closestHostile) {
+			target = closestHostile;
+		} else if(closestStructure) {
+			target = closestStructure;
+		} else {
+			target = false;
+		}
+		if(target){
+			if(creep.attack(target) == ERR_NOT_IN_RANGE){
+				creep.moveTo(target);
+			} 
+			creep.memory.targetId = target.id;
 		} else {
 			creep.moveTo(Game.flags.target0);
 		}			
