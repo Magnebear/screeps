7066424ee6649a0f603efea7138be84d1f916a34
Magnebear <github.mail@cyfor.no>
2017-09-13 22:01:53 +0200

    a

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ module.exports.loop = function () {
 		var builders2 = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder2');
 		var roles = {}
 		for(var name in Game.creeps) {
-			if(roles[Game.creeps[name].memory.role])++
+			(roles[Game.creeps[name].memory.role])++
 		}
 		console.log(roles)
 	
