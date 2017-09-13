210dbee6b2d6d147df0f44cd847a6befbf5eb2bc
Magnebear <github.mail@cyfor.no>
2017-09-13 22:15:33 +0200

    a

--- main.js
+++ main.js
@@ -70,11 +70,7 @@ module.exports.loop = function () {
 		
 		var roles = {}
 		for(var name in Game.creeps) {
-			if(roles[Game.creeps[name].memory.role]){
-				roles[Game.creeps[name].memory.role] = roles[Game.creeps[name].memory.role] + 1
-			} else {
-				roles[Game.creeps[name].memory.role] = 0
-			}
+				roles[Game.creeps[name].memory.role] ++
 		}
 		
 		for(var role in roles) {
