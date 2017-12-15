4fff1e963fa620fc8b6fbc059cbf695cacda2c65
Magnebear <github.mail@cyfor.no>
2017-12-15 16:14:45 +0100

    a

--- main.js
+++ main.js
@@ -164,16 +164,20 @@ module.exports.loop = function () {
 		
 		
 		//Check if creeps are alive
-		if(Game.creeps[Memory.room.exoMiner1]){
-			console.log("Exominer1 Alive");
-		} else {
-			console.log("Exominer1 dead");
+		if(Memory.room.exoMiner1){
+			if(Game.creeps[Memory.room.exoMiner1]){
+				console.log("Exominer1 Alive");
+			} else {
+				console.log("Exominer1 dead");
+			}
 		}
 		
-		if(Game.creeps[Memory.room.exoMiner2]){
-			console.log("Exominer2 Alive");
-		} else {
-			console.log("Exominer2 dead");
+		if(Memory.room.exoMiner2){
+			if(Game.creeps[Memory.room.exoMiner2]){
+				console.log("Exominer2 Alive");
+			} else {
+				console.log("Exominer2 dead");
+			}
 		}
 		
 		
