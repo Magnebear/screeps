821c7ffaaa020d6d7792417e70628f6752c11d70
Magnebear <github.mail@cyfor.no>
2017-12-15 10:10:29 +0100

    a

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ var externalBuilderCreep = [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE]
 var superTransporterCreep = [MOVE,MOVE,MOVE,MOVE,MOVE,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY]
 
 var maxHarvester = 2;
-var maxBuilders = 2;
+var maxBuilders = 3;
 var maxUpgraders = 3;
 var maxRepair = 1;
 
