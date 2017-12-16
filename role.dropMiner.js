6466afa90b4478f68d86eca195693dc25b81aa86
Magnebear <github.mail@cyfor.no>
2017-12-16 12:31:17 +0100

    a

--- role.dropMiner.js
+++ role.dropMiner.js
@@ -3,7 +3,6 @@ module.exports  = {
     /** @param {Creep} creep **/
     run: function(creep) {
         var source = Game.getObjectById(creep.memory.srcID);
-		console.log("Source: "+source)
 		if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
 				creep.travelTo(source);
 		}
