ed007e90e0c8cc7ca99ac4bcfb985e3e0b8fc00e
Magnebear <github.mail@cyfor.no>
2017-12-17 10:19:30 +0100

    a

--- role.scout.js
+++ role.scout.js
@@ -2,7 +2,7 @@ var Traveler = require('Traveler');
 module.exports  = {
     /** @param {Creep} creep **/
     run: function(creep) {
-		creep.travelTo(creep.memory.targetRoom)
+		creep.travelTo(Game.getObjectById(creep.memory.targetRoom))
     },
 	create: function (name, creepBody, target, spawn){
 		var newName = Game.spawns[spawn].createCreep(creepBody, name, {role: 'scout', targetRoom:target});
