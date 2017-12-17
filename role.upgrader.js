acfe1af6358bdd07181bcc506accc48ae8118536
Magnebear <github.mail@cyfor.no>
2017-12-17 09:21:39 +0100

    a

--- role.upgrader.js
+++ role.upgrader.js
@@ -25,13 +25,13 @@ var roleUpgrader = {
             creep.upgradeController(creep.room.controller)
         } else {
             var target = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
-			var containers = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 500}});
+			var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 500}});
 			if(target){
 				creep.moveTo(target);
 				creep.pickup(target);
-			} else if (containers.length > 0){ 
-				if(creep.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				   creep.travelTo(containers[0]);
+			} else if (container){ 
+				if(creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				   creep.travelTo(container);
 				}
 			} else {
 			//Idle
