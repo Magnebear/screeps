d29fb6fe32a20947a25c1c7f77887d2850e99fe6
Magnebear <github.mail@cyfor.no>
2017-12-17 10:43:41 +0100

    a

--- role.scout.js
+++ role.scout.js
@@ -2,7 +2,22 @@ var Traveler = require('Traveler');
 module.exports  = {
     /** @param {Creep} creep **/
     run: function(creep) {
-		creep.moveTo(new RoomPosition(25, 25, creep.memory.targetRoom))
+		var tgt = creep.memory.targetRoom
+		creep.moveTo(new RoomPosition(25, 25, tgt))
+		if(Game.rooms[tgt] == undefined){
+			
+		} else {
+			var sources = Game.rooms[tgt].find(FIND_SOURCES);
+			sources.forEach(function (source){
+				Memory.exoRooms.tgt.sources.source = {}
+				Memory.exoRooms.tgt.sources.source.pos = source.RoomPosition
+				Memory.exoRooms.tgt.sources.source.pos = source.id
+			})
+			
+		
+		}
+		
+		
     },
 	create: function (name, creepBody, target, spawn){
 		var newName = Game.spawns[spawn].createCreep(creepBody, name, {role: 'scout', targetRoom:target});
