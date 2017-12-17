61a5da0ee8e3dc608a7205f06a91e31ef0a2a6e2
Magnebear <github.mail@cyfor.no>
2017-12-17 08:47:44 +0100

    a

--- role.transporter.js
+++ role.transporter.js
@@ -48,7 +48,7 @@ var roleTransporter = {
             }
         } else {
 			var target = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
-			var containers = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 0 }});
+			var containers = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 100 }});
 			if(target){
 				creep.moveTo(target);
 				creep.pickup(target);
