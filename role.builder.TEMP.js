4a36ea2286944741241ca2ff25c61491df19928e
Magnebear <github.mail@cyfor.no>
2017-12-17 08:56:00 +0100

    a

--- role.builder.TEMP.js
+++ role.builder.TEMP.js
@@ -25,14 +25,14 @@ var roleBuilderTEMP = {
 			}
 	    } else {
 			var target = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
-			var containers = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 100 }});
+			var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 100 }});
 
 			if(target) {
 				creep.moveTo(target);
 				creep.pickup(target);
-			}else if (containers.length > 0){
-				if(creep.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				   creep.travelTo(containers[0]);
+			}else if (container){
+				if(creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				   creep.travelTo(container);
 				}
 			}
 		}
