8d4554f5c5470568d506f52d52a7977b7805b95f
Magnebear <github.mail@cyfor.no>
2017-12-17 11:37:13 +0100

    a

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ var exoMuleCreep = [MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,CARRY,CARRY,CARRY,CARRY,CARRY,
 
 var maxHarvester = 0;
 var maxBuilders = 3;
-var maxUpgraders = 7;
+var maxUpgraders = 6;
 var maxRepair = 2;
 var maxTransporters = 3;
 
