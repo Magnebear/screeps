2799758421e1f85a4075c9765e81792c08ec295e
Magnebear <github.mail@cyfor.no>
2017-12-17 19:20:29 +0100

    a

--- role.repair.js
+++ role.repair.js
@@ -1,5 +1,5 @@
 var Traveler = require('Traveler');
-const maxHits = 15000;
+const maxHits = 20000;
 var roleRepair = {
     run: function(creep) {
         if(creep.memory.repairTarget==null || !creep.memory.repairTarget) {
