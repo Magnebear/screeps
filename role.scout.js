aa1382bb72f139fbdbceacfdcb28e16dd57d2312
Magnebear <github.mail@cyfor.no>
2017-12-17 11:07:46 +0100

    a

--- role.scout.js
+++ role.scout.js
@@ -8,15 +8,8 @@ module.exports  = {
 			
 		} else {
 			console.log("Saving sources")
-			var srcs = Game.rooms[tgt].find(FIND_SOURCES);
-			console.log(srcs)
-			srcs.forEach(function (src){
-				Memory.exoRooms[tgt].sources.src = {}
-				Memory.exoRooms[tgt].sources.src.rPos = src.RoomPosition
-				Memory.exoRooms[tgt].sources.src.sID = src.id
-			})
+			Memory.exoRooms[tgt].sources = Game.rooms[tgt].find(FIND_SOURCES);
 			
-		
 		}
 		
 		
