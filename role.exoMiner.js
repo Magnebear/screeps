a627bf3317cc8362a979f877f4d6521fe73299ad
Magnebear <github.mail@cyfor.no>
2017-12-18 10:18:16 +0100

    a

--- role.exoMiner.js
+++ role.exoMiner.js
@@ -3,24 +3,27 @@ var Traveler = require('Traveler');
 module.exports  = {
     /** @param {Creep} creep **/
     run: function(creep) {
-		var startCpu = Game.cpu.getUsed();
+		//var startCpu = Game.cpu.getUsed();
 		
 		rm = creep.memory.room
 		src = creep.memory.source
-		
-		console.log(creep.name)
-		console.log(rm)
-		console.log(src)
-		
+
 		try{
 			creep.harvest(Game.getObjectById(Memory.exoRooms[rm].sources[src].id));
 		} catch (e) {
-			//creep.moveTo(new RoomPosition(Memory.exoRooms[rm].sources[src].pos.x, Memory.exoRooms[rm].sources[src].pos.y, rm));
+			creep.moveTo(new RoomPosition(Memory.exoRooms[rm].sources[src].pos.x, Memory.exoRooms[rm].sources[src].pos.y, rm));
+		} finally (f){
+			console.log("Exo miner error")
+			console.log(creep.name)
+			console.log(rm)
+			console.log(src)
+			console.log(e)
+			console.log(f)
 		}
-		console.log('elapsed :', Game.cpu.getUsed() - startCpu);
+		//console.log('elapsed :', Game.cpu.getUsed() - startCpu);
 		
     },
-	create: function (creepBody, name,  rm, src, spawn){
+	create: function (creepBody, name, rm, src, spawn){
 		var newName = Game.spawns[spawn].createCreep(creepBody, name, {role: 'exoMiner',room:rm, source:src});
 		return newName;
 	}
