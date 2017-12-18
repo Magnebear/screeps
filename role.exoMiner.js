264142b12a5c8fa05cfb9f5f1aeb7b9ad26ce7c9
Magnebear <github.mail@cyfor.no>
2017-12-18 09:58:54 +0100

    a

--- role.exoMiner.js
+++ role.exoMiner.js
@@ -15,7 +15,7 @@ module.exports  = {
 		try{
 			creep.harvest(Game.getObjectById(Memory.exoRooms[rm].sources[src].id));
 		} catch (e) {
-			creep.moveTo(new RoomPosition(Memory.exoRooms[rm].sources[src].pos.x, Memory.exoRooms[rm].sources[src].pos.y, rm));
+			//creep.moveTo(new RoomPosition(Memory.exoRooms[rm].sources[src].pos.x, Memory.exoRooms[rm].sources[src].pos.y, rm));
 		}
 		console.log('elapsed :', Game.cpu.getUsed() - startCpu);
 		
