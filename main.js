303b24bd1f9036755c5c42bbf963d772cc50946d
Magnebear <github.mail@cyfor.no>
2017-12-20 12:59:52 +0100

    a

--- main.js
+++ main.js
@@ -216,12 +216,12 @@ module.exports.loop = function () {
 		//new RoomVisual('W1N1').poly(path, {stroke: '#fff', strokeWidth: .15,opacity: .2, lineStyle: 'dashed'});
 	}
 	
-	if(Memory.showPath == true){
+/* 	if(Memory.showPath == true){
 		for(var propt in Memory.sortedPath){
 			console.log(sortedPath[propt][0].roomName)
 			new RoomVisual(sortedPath[propt][0].roomName).poly(sortedPath[propt], {roomPath: '#fff', strokeWidth: .15,opacity: .2, lineStyle: 'dashed'});
 		};
-	}
+	} */
 	
 }
 
