12076f2a83fedfc547d931c519777cec2b2a27fe
Magnebear <github.mail@cyfor.no>
2017-12-22 15:02:14 +0100

    a

--- main.js
+++ main.js
@@ -139,12 +139,9 @@ module.exports.loop = function () {
 				
 				console.log(i)
 				console.log(Game.spawns["Spawn1"].room.storage.pos)
-				console.log(Memory.exoRooms[exoRoom].sources[i].pos.x)
-				console.log(Memory.exoRooms[exoRoom].sources[i].pos.y)
-				console.log(Memory.exoRooms[exoRoom].sources[i].pos.roomName)
-				
+				pos = Memory.exoRooms[exoRoom].sources[i].pos
 				//Optimal path, ignoring swamps
-				var path = PathFinder.search(Game.spawns["Spawn1"].room.storage.pos,{pos:new RoomPosition(Memory.exoRooms[exoRoom].sources[i].pos.x,Memory.exoRooms[exoRoom].sources[i].pos.y,Memory.exoRooms[exoRoom].sources[i].pos.roomName),range:1},{swampCost:1});	
+				var path = PathFinder.search(Game.spawns["Spawn1"].room.storage.pos,{pos:new RoomPosition(pos.x,pos.y,pos.roomName),range:1},{swampCost:1});	
 				//Create road construction site for each road segment
 				for(var j=0; j<path.length;j++){
 						console.log(j)
