795e9c8b5c6a76d27ec53f13a63650496f72d513
Magnebear <github.mail@cyfor.no>
2017-12-23 11:21:03 +0100

    a

--- role.exoJanitor.js
+++ role.exoJanitor.js
@@ -27,8 +27,21 @@ module.exports = {
 					creep.memory.mode = "build"
 					creep.say("Build")
 				} else {
-					creep.memory.mode = "idle"
-					creep.say("Idle")
+					var repairTarget = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+						filter: (structure) => {
+							return(structure.structureType == STRUCTURE_ROAD ||
+								structure.structureType == STRUCTURE_CONTAINER) &&
+								structure.hits < structure.hitsMax
+							}
+					});
+					if (repairTarget) {
+						creep.memory.target = repairTarget.id
+						creep.memory.mode = "repair"
+						creep.say("Repair")
+					} else {
+						creep.memory.mode = "idle"
+						creep.say("idle")
+					}					
 				}
 			}
 		}
@@ -58,6 +71,7 @@ module.exports = {
 				}
 				break;
 			case "idle":
+				creep.moveTo(new RoomPosition(25,25,creep.memory.exoRoom))
 				break;
 		}
 	},
