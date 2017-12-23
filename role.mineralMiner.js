4fabf22f9d200492b840edb8d880373993b57fe1
Magnebear <github.mail@cyfor.no>
2017-12-23 13:50:47 +0100

    a

--- role.mineralMiner.js
+++ role.mineralMiner.js
@@ -4,6 +4,14 @@ module.exports  = {
     /** @param {Creep} creep **/
     run: function(creep) {	
 		mineral = creep.room.find(FIND_MINERALS)[0]
+/* 		container = mineral.pos.findClosestByRange(FIND_STRUCTURES, {
+			filter: (structure) => {structure.structureType == STRUCTURE_CONTAINER}
+		}); */
+/* 		
+		creep.travelTo(container)
+		if(container.store[RESOURCE_ENERGY] < 2000
+		
+		 */
 		if(_.sum(creep.carry) < creep.carryCapacity && mineral.mineralAmount > 0){
 			if(creep.harvest(mineral) != 0){
 				creep.travelTo(mineral)
