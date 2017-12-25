55cb619f64e79499cb6388eb6f59df4290aa1fe2
Magnebear <github.mail@cyfor.no>
2017-12-25 08:40:56 +0100

    a

--- main.js
+++ main.js
@@ -355,8 +355,8 @@ function exoRoomControll(){
 				if(Game.creeps["exoJanitor"+exoRoom] == undefined){
 					console.log("Checking if janitor is needed in room "+exoRoom)
 					var startCpu = Game.cpu.getUsed();
-					var constructionSites = Game.rooms[exoRoom].find(FIND_CONSTRUCTION_SITES)
-					var repairSites = Game.rooms[exoRoom].find(FIND_STRUCTURES, {
+					//var constructionSites = Game.rooms[exoRoom].find(FIND_CONSTRUCTION_SITES)
+					//var repairSites = Game.rooms[exoRoom].find(FIND_STRUCTURES, {
 						filter: (structure) => {
 							return(structure.structureType == STRUCTURE_ROAD
 							&& structure.hits < (structure.hitsMax/2))}
